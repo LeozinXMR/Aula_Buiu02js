092c618c84923a014819eab798251c4a8a2039b4
LeozinXMR <144696317+LeozinXMR@users.noreply.github.com>
2024-02-16 20:38:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log (total)
+var idade = 'dezenove 19'
+console.log (isNaN(idade));
+var valor = '300BRL'
+console.log (isNaN(valor));
+var somar = +'200' + 50;
+console.log (somar)
+var incremento = 5;
+console.log (++incremento);
+var numero = '80';
+var unidade = 'kg';
+var peso = numero /2;
+var pesodivpordois = peso + unidade;
+console.log(pesodivpordois)
